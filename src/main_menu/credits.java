7af13f11ea8e72802d13e47565426b2ae2acedb9
zovippro1996 <trunglepham1202@gmail.com>
2017-04-17 20:38:21 +0700

    refresh

--- src/main_menu/credits.java
+++ src/main_menu/credits.java
@@ -20,8 +20,33 @@ import java.awt.event.ActionEvent;
  * @author zovippro1996
  */
 
-public class credits {
+public class credits extends JFrame implements ActionListener {
     
-    
+    public credits()
+    {
+        JPanel pnlButton = new JPanel();
+        
+        //make sure the program exits when the frame closes
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Credits");
+        setSize(860,590);
+        
+        
+        //This will center the JFrame in the middle of the screen
+        setLocationRelativeTo(null);
+        pnlButton.setLayout(null);
+        
+        
+        //Add Panel to the Main Menu
+        add(pnlButton);
+      
+        //Make the Main Menu Appear
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
