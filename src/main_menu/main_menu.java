6401f7d8e556744669ec7fe2094485f5d110768a
zovippro1996 <trunglepham1202@gmail.com>
2017-04-17 19:34:52 +0700

    Update Function in Menu

--- src/main_menu/main_menu.java
+++ src/main_menu/main_menu.java
@@ -17,13 +17,14 @@ import java.awt.BorderLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-public class main_menu extends JFrame {
+public class main_menu extends JFrame implements ActionListener {
     
     //Note: Typically the main method will be in a
     //separate class. As this is a simple one class
     //example it's all in the one class.
     public static void main(String[] args) {
         
+        //Call out the main_menu Constructor
         new main_menu();
     }
 
@@ -34,32 +35,67 @@ public class main_menu extends JFrame {
         //make sure the program exits when the frame closes
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("Main Menu");
-        setSize(860,640);
+        setSize(860,590);
+        
         //This will center the JFrame in the middle of the screen
         setLocationRelativeTo(null);
         pnlButton.setLayout(null);
     
+        //Add Create Project Button to the Main Menu
         JButton create_button = new JButton("Create Project");
+        create_button.addActionListener(this);
+        create_button.setActionCommand("New");
         create_button.setBounds(330,100,200,50);
         
+        //Add Load Button to the Main Mennu
         JButton load_button = new JButton("Load Project");
+        load_button.addActionListener(this);
+        load_button.setActionCommand("Load");
         load_button.setBounds(330, 175, 200, 50);
         
+        //Add Credit Button to the Main Menu
         JButton credits_button = new JButton("Credits");
+        credits_button.addActionListener(this);
+        credits_button.setActionCommand("Credits");
         credits_button.setBounds(330, 250, 200, 50);
         
+        //Initialize Exit Button
         JButton exit_button = new JButton("Exit");
+        exit_button.addActionListener(this);
+        exit_button.setActionCommand("Exit");
         exit_button.setBounds(330, 325, 200, 50);
         
-   
+        //Add 4 initialize Button to the Panel
         pnlButton.add(create_button);
         pnlButton.add(load_button);
         pnlButton.add(credits_button);
         pnlButton.add(exit_button);
         
+        
+        //Add Panel to the Main Menu
         add(pnlButton);
       
+        //Make the Main Menu Appear
         setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String action = e.getActionCommand();
+	        if (action.equals("New")) {
+	            System.out.println("Not Support Yet");}
+                
+                else if (action.equals("Load")) {
+                    System.out.print("Not support yet");
+                }
+                
+                else if (action.equals("Credits")) {
+                    System.out.print("A Dedicated Project from Computer Graphic Course by \n Pham Le Trung \n Le Van Tu");
+                }
+                
+                else if (action.equals("Exit")){
+                    System.exit(0);
+                }
     }
     
 }
