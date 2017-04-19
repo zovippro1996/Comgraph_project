d27d58d84747b18d49fb6eb48d8043893be06b36
zovippro1996 <trunglepham1202@gmail.com>
2017-04-19 19:59:04 +0700

    Add Background to Main + Cre

--- src/main_menu/credits.java
+++ src/main_menu/credits.java
@@ -14,6 +14,8 @@ import javax.swing.JList;
 import java.awt.BorderLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.net.URL;
+import javax.swing.ImageIcon;
 
 /**
  *
@@ -27,19 +29,25 @@ public class credits extends JFrame implements ActionListener {
         //make sure the program exits when the frame closes
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("Credits");
-        setSize(860, 590);
+        setSize(1366, 768);
 
         //This will center the JFrame in the middle of the screen
         setLocationRelativeTo(null);
         pnlButton.setLayout(null);
 
+        //URL of background image
+        URL url = getClass().getClassLoader().getResource("main_menu/resources/images/main-menu-bg.jpg");
+        JLabel background = new JLabel(new ImageIcon(url));
+        background.setBounds(0, 0, 1366, 768);
+
         //Add Back to Main Menu Button to Credit
         JButton back_button = new JButton("Back to Main");
         back_button.addActionListener(this);
         back_button.setActionCommand("Back");
-        back_button.setBounds(330,100,200,50);
-        
+        back_button.setBounds(85, 600, 200, 50);
+
         pnlButton.add(back_button);
+        pnlButton.add(background);
         //Add Panel to the Main Menu
         add(pnlButton);
 
--- src/main_menu/main_menu.java
+++ src/main_menu/main_menu.java
@@ -20,101 +20,106 @@ import com.sun.j3d.utils.geometry.Box;
 import com.sun.j3d.utils.universe.*;
 import com.sun.j3d.utils.geometry.Sphere;
 import com.sun.j3d.utils.image.TextureLoader;
+import java.awt.Container;
+import java.awt.FlowLayout;
 import javax.media.j3d.*;
 import javax.vecmath.*;
 import java.awt.GraphicsConfiguration;
+import java.net.URL;
+import javax.swing.ImageIcon;
 
 public class main_menu extends JFrame implements ActionListener {
-    
+
     //Note: Typically the main method will be in a
     //separate class. As this is a simple one class
     //example it's all in the one class.
     public static void main(String[] args) {
-        
+
         //Call out the main_menu Constructor
         new main_menu();
     }
 
-    public main_menu()
-    {
-        JPanel pnlButton = new JPanel();
-        
+    public main_menu() {
+
         //make sure the program exits when the frame closes
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("Main Menu");
-        setSize(860,590);
-        
+        setSize(1366, 768);
+
+        JPanel pnlButton = new JPanel();
+
         //This will center the JFrame in the middle of the screen
         setLocationRelativeTo(null);
+
+        //Get allign Button
         pnlButton.setLayout(null);
-    
+
+        //URL of background image
+        URL url = getClass().getClassLoader().getResource("main_menu/resources/images/main-menu-bg.jpg");
+        JLabel background = new JLabel(new ImageIcon(url));
+        background.setBounds(0, 0, 1366, 768);
+
         //Add Create Project Button to the Main Menu
         JButton create_button = new JButton("Create Project");
         create_button.addActionListener(this);
         create_button.setActionCommand("New");
-        create_button.setBounds(330,100,200,50);
-        
+        create_button.setBounds(583, 200, 200, 50);
+
         //Add Load Button to the Main Mennu
         JButton load_button = new JButton("Load Project");
         load_button.addActionListener(this);
         load_button.setActionCommand("Load");
-        load_button.setBounds(330, 175, 200, 50);
-        
+        load_button.setBounds(583, 300, 200, 50);
+
         //Add Credit Button to the Main Menu
         JButton credits_button = new JButton("Credits");
         credits_button.addActionListener(this);
         credits_button.setActionCommand("Credits");
-        credits_button.setBounds(330, 250, 200, 50);
-        
+        credits_button.setBounds(583, 400, 200, 50);
+
         //Initialize Exit Button
         JButton exit_button = new JButton("Exit");
         exit_button.addActionListener(this);
         exit_button.setActionCommand("Exit");
-        exit_button.setBounds(330, 325, 200, 50);
-        
+        exit_button.setBounds(583, 500, 200, 50);
+
         //Add 4 initialize Button to the Panel
         pnlButton.add(create_button);
         pnlButton.add(load_button);
         pnlButton.add(credits_button);
         pnlButton.add(exit_button);
-        
-        
+        pnlButton.add(background);
         //Add Panel to the Main Menu
         add(pnlButton);
-      
-        //Make the Main Menu Appear
+
+//Make the Main Menu Appear
         setVisible(true);
+
     }
-    
-    
+
     //Set action when receive command from Buttons
     @Override
     public void actionPerformed(ActionEvent e) {
         String action = e.getActionCommand();
-                //Action for Create Button
-	        if (action.equals("New")) {
-	            first_j3d J3D;
-		//Link to first_j3d	
+        //Action for Create Button
+        if (action.equals("New")) {
+            first_j3d J3D;
+            //Link to first_j3d	
             J3D = new first_j3d();
             J3D.setVisible(true);
-}
-                
-                //Action for Load Button
-                else if (action.equals("Load")) {
-                    System.out.print("Not support yet");
-                }
-                
-                //Action for Credits Button
-                else if (action.equals("Credits")) {
-                    credits credit_main = new credits();
-                    credit_main.setVisible(true);
-                    this.dispose();
-                }
-                
-                //Action for Exit Button
-                else if (action.equals("Exit")){
-                    System.exit(0);
-                }
+            this.dispose();
+        } //Action for Load Button
+        else if (action.equals("Load")) {
+            System.out.print("Not support yet");
+        } //Action for Credits Button
+        else if (action.equals("Credits")) {
+            credits credit_main = new credits();
+            credit_main.setVisible(true);
+            this.dispose();
+        } //Action for Exit Button
+        else if (action.equals("Exit")) {
+            System.exit(0);
+        }
     }
-    
+
 }
